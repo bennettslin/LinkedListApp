8d6135db109ce02efe28c9bbcf0c31602a6420a8
Bennett Lin <bennettslin@gmail.com>
2014-02-24 12:49:17 -0800

    Initial Commit

---
+++ LinkedListApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  LinkedListApp
+//
+//  Created by Bennett Lin on 2/24/14.
+//  Copyright (c) 2014 Bennett Lin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ LinkedListApp/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  LinkedListApp
+//
+//  Created by Bennett Lin on 2/24/14.
+//  Copyright (c) 2014 Bennett Lin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
